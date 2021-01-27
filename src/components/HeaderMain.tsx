c1d3e282d35b2741cb122922af841353481a6dd0
PauloMisaki <ph.sorata@gmail.com>
2021-01-27 14:21:53 -0300

    another change to logo

--- src/components/HeaderMain.tsx
+++ src/components/HeaderMain.tsx
@@ -1,7 +1,7 @@
 export default function HeaderMain() {
     return (
         <div className="fixed z-10 flex flex-row justify-between w-full py-4 border-t-4 bg-none box-nav">
-            <div className="mt-4 ml-32">
+            <div className="mt-1 ml-8">
                 <img src="logo.png" alt="" className=""/>
             </div>
             <div className="mt-4 mr-16 space-x-12">
